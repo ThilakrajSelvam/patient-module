fdfe0df30dd16caf767abadc63c5c3c838c706ee
ThilakrajSelvam <thilak6991@gmail.com>
2020-12-10 17:23:29 +0530

    consul service discovery implemented

--- src/main/java/com/playground/patientmodule/PatientModuleApplication.java
+++ src/main/java/com/playground/patientmodule/PatientModuleApplication.java
@@ -13,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableCaching
+@EnableDiscoveryClient
 public class PatientModuleApplication {
 
     public static void main(String[] args) {
